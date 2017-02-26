e291373eab465fab5d225e30555d9aee06f16e6d
jffielder <jfielder0@gmail.com>
2017-02-26 11:02:42 -0500

    test branching

--- js/script.js
+++ js/script.js
@@ -3,22 +3,6 @@ $(document).ready(function(){
 
 	//var rows = prompt("input");
 
-	var rows = 16;
-
-	for (var i = 0; i < rows; i++) {
-		//$('.container').apend("<div>test</div>");
-		$(".container").append("<div class=columns id=blok></div>");
-	}
-
-	for (var j = 0; j < rows ; j++) {
-		//$(".container div").append("<div>set</div>");
-
-		$(".columns").append("<div class=rows id=blok></div>");
-	}
-	
-	$(".rows").hover(function(){
-		$(this).addClass("colorOn");
-	});	
 	
 
 });
