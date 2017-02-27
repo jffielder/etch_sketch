d83bd036f4978dbae2d27ea3b04890afb487575d
jffielder <jfielder0@gmail.com>
2017-02-26 23:03:56 -0500

    Done

--- js/script.js
+++ js/script.js
@@ -1,17 +1,68 @@
 
 $(document).ready(function(){
 
-	//var rows = prompt("input");
-	for (var i = 256; i > 0; i--) {
-		$(".container").append("<div class='box'></div>");	
+	var boxNum = 256;
+
+	makeBoxes(boxNum);
+
+	hoverOn();
+
+	clearOn();
+
+	resizeOn();
+
+	//make boxes
+	function makeBoxes(boxNumber, w){
+		
+		for (var i = boxNumber; i > 0; i--) {
+			$(".container").append("<div class='box'></div>");	
+			console.log("HERE");
+		}
+
+		$(".box").width(w);
+		$(".box").height(w);
 	}
-	
-	$(".box").hover(function(){
-		$(this).addClass("hoverOn");
-	});
+
+	//delete boxes
+
+	function delBoxes(){
+		$(".box").remove();	
+	}
+
+	//hover function	
+	function hoverOn(){
+		$(".box").hover(function(){
+			$(this).addClass("hoverOn");
+		});
+	}
+
+	//clear button
+	function clearOn(){
+		$("button#clear").click(function(){
+			$(".box").removeClass("hoverOn");
+		});
+	}
+
+	//resize button
+	function resizeOn(){
+		$("button#resize").click(function(){
+
+			var boxSize = Number(prompt("Input"));
+
+			delBoxes();
+
+			//make width of each box 400/input
+
+			var w = (400 / boxSize)
+
+			var area = boxSize*boxSize;
+
+			makeBoxes(area, w);
+
+			hoverOn();
+
+		});
+	}
 
 });//rdy
 
-function clearFunction(){
-	$(".box").removeClass("hoverOn");
-}
