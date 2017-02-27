6458a2105cfe654e2f3f8f3b824f61f8e5c3999d
jffielder <jfielder0@gmail.com>
2017-02-26 21:21:01 -0500

    working

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@
 $(document).ready(function(){
 
 	//var rows = prompt("input");
-	for (var i = 256; i >= 0; i--) {
+	for (var i = 256; i > 0; i--) {
 		$(".container").append("<div class='box'></div>");	
 	}
 	
+	$(".box").hover(function(){
+		$(this).addClass("hoverOn");
+	});
 
-});
+});//rdy
+
+function clearFunction(){
+	$(".box").removeClass("hoverOn");
+}
